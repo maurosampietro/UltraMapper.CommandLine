a2a4c6ae106ea7b19f5311404531b6bee84a8b74
mauro sampietro <mauro.sampietro@gmail.com>
2020-11-26 18:25:41 +0100

    collection of nullables

--- UltraMapper.CommandLine.UnitTest/Nullables.cs
+++ UltraMapper.CommandLine.UnitTest/Nullables.cs
@@ -10,10 +10,13 @@ namespace UltraMapper.CommandLine.UnitTest
     [TestClass]
     public class Nullables
     {
-        public class Commands
+        public class NullableProperty
         {
             public int? Option { get; set; }
+        }
 
+        public class NullableMethod
+        {
             [Option( IsIgnored = true )] public bool IsExecuted { get; set; }
             [Option( IsIgnored = true )] public int? MethodParam { get; private set; } = null;
 
@@ -24,22 +27,57 @@ namespace UltraMapper.CommandLine.UnitTest
             }
         }
 
+        public class NullableCollectionProperty
+        {
+            public IEnumerable<int?> Option { get; set; }
+        }
+
+        public class NullableCollectionMethod
+        {
+            [Option( IsIgnored = true )] public bool IsExecuted { get; set; }
+            [Option( IsIgnored = true )] public IEnumerable<int?> MethodParam { get; private set; } = null;
+
+            public void Method( IEnumerable<int?> option )
+            {
+                this.IsExecuted = true;
+                this.MethodParam = option;
+            }
+        }
+
         [TestMethod]
         public void SetNullableProperty()
         {
-            string args = $"--{nameof( Commands.Option )} 11";
-            var parsed = CommandLine.Instance.Parse<Commands>( args );
+            string args = $"--{nameof( NullableProperty.Option )} 11";
+            var parsed = CommandLine.Instance.Parse<NullableProperty>( args );
             Assert.IsTrue( parsed.Option == 11 );
         }
 
         [TestMethod]
         public void SetNullableMethodParameter()
         {
-            string args = $"--{nameof( Commands.Method )} 11";
-            var parsed = CommandLine.Instance.Parse<Commands>( args );
+            string args = $"--{nameof( NullableMethod.Method )} 11";
+            var parsed = CommandLine.Instance.Parse<NullableMethod>( args );
 
-            Assert.IsTrue( parsed.IsExecuted ); 
+            Assert.IsTrue( parsed.IsExecuted );
             Assert.IsTrue( parsed.MethodParam == 11 );
+        }
+
+        [TestMethod]
+        public void SetNullableCollectionProperty()
+        {
+            string args = $"--{nameof( NullableCollectionProperty.Option )} [11 13]";
+            var parsed = CommandLine.Instance.Parse<NullableCollectionProperty>( args );
+            Assert.IsTrue( parsed.Option.SequenceEqual( new int?[] { 11, 13 } ) );
+        }
+
+        [TestMethod]
+        public void SetNullableCollectionMethodParameter()
+        {
+            string args = $"--{nameof( NullableCollectionMethod.Method )} [11 13]";
+            var parsed = CommandLine.Instance.Parse<NullableCollectionMethod>( args );
+
+            Assert.IsTrue( parsed.IsExecuted );
+            Assert.IsTrue( parsed.MethodParam.SequenceEqual( new int?[] { 11, 13 } ) );
         }
     }
 }
